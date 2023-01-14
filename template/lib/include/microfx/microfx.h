2fe7d3e6a2a1baf780359b01eeb16d7137908d92
mibi88 <mibikart@gmail.com>
2023-01-14 17:40:01 +0100

    Updated lib in template/

--- template/lib/include/microfx/microfx.h
+++ template/lib/include/microfx/microfx.h
@@ -9,37 +9,44 @@ enum {SWHITE = 0, SBLACK};
 
 /* void sclear(void);
 
-Clears the screen in white.
+Clears the VRAM in white.
 */
 
 void sclear(void);
 
-/*
+/* void supdate(void);
 
+Put the VRAM on the screen.
 */
 
 void supdate(void);
 
-/*
+/* void srect(int x1, int y1, int x2, int y2);
 
+Draws a white rectangle with a black border of one pixel from (x1, y1) to (x2, y2)
+in the VRAM.
 */
 
 void srect(int x1, int y1, int x2, int y2);
 
-/*
+/* void spixel(int x, int y, int color);
 
+Set the pixel at (x, y) in the color color in the VRAM.
+The available colors are SWHITE or SBLACK.
 */
 
 void spixel(int x, int y, int color);
 
-/*
+/* void stext(int x, int y, char *text);
 
+Puts the text text at (x, y) on the screen using casio default font.
 */
 
 void stext(int x, int y, char *text);
 
-/*
+/* void slocate(int x, int y, char *text);
 
+Works like the Locate function of CASIO Basic.
 */
 
 void slocate(int x, int y, char *text);
@@ -48,20 +55,50 @@ void slocate(int x, int y, char *text);
 
 enum {KFAST = 0, KLONG};
 
+/* int kisdown(void);
+
+Returns a positive int if a key is down.
+Returns 0 if there is no key down.
+*/
 int kisdown(void);
+
+/* int kcheck(int key, int type);
+
+/!\ Not working /!\
+
+Checks if the key key is down.
+
+int key if 0x(1 byte for the column)(1 byte for the row).
+Based on the syscalls 0x24B/0x24C.
+
+Type is KFAST or KLONG.
+KFAST : Syscall 0x24B is used.
+KLONG : Syscall 0x24C is used.
+*/
 int kcheck(int key, int type);
+
+/* int kgetkey(void);
+
+Works like the Getkey function of CASIO Basic.
+Returns the pressed key.
+*/
 int kgetkey(void);
 
 /******* TOOLS *******/
 
-/*
+/* void itoa(int n, char *buffer);
 
+Put a string of the int n in buffer.
 */
 
 void itoa(int n, char *buffer);
 
 /******* TIME *******/
 
+/* void tsleep_ms(int ms);
+
+Wait ms miliseconds.
+*/
 void tsleep_ms(int ms);
 
 #endif
