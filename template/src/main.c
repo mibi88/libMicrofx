2b1f4fa1b3c52f741ffc7151c5c925866d346a33
mibi88 <mibikart@gmail.com>
2023-01-21 12:22:43 +0100

    Updated template.

--- template/src/main.c
+++ template/src/main.c
@@ -2,7 +2,11 @@
 
 int main(void) {
 	while(kisdown());
-	stext(1, 1, "A Microfx Add-in !");
-	while(!kisdown());
+	stext(1, 1, "A Microfx Add-in !", SBLACK);
+	/* Waits that the user presses EXIT. */
+	int key = 0;
+	while(key != KCEXIT){
+		key = kgetkey();
+	}
 	return 1;
 }
